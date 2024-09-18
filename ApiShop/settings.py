11672cefab6c6b2398313b4a7fa26409b0786716
miskaso <qwertyclop@gmail.com>
2024-09-18 19:30:28 +0600

    Docker

--- ApiShop/settings.py
+++ ApiShop/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-h54-egdz(urzqb5elk-&pal6udbc62g)km9j(y+$((l5bqox(=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '176.124.209.174', '127.0.0.1']
 
 
 # Application definition
---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.11
+
+WORKDIR /app
+
+COPY requirements.txt / app /
+
+RUN pip install -r /app/requirements.py
+
+COPY . /app
+
+CMD ["python", "manage.py", "runserver", "0.0.0.0:80"]
